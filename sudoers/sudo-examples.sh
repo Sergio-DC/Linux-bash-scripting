ea02f2eb4a4fc3c83450b66fbc3e938912cb8835
Sergio-DC <sgdc377@gmail.com>
2021-01-13 19:09:54 -0500

    sudoers examples

---
+++ sudoers/sudo-examples.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Write the following commands inside /etc/sudoers
+
+#Syntax sudo line:  user machine = (user2) commnad
+
+#This command adds user jerry to the list of sudoers, with all the privileges
+jerry ALL= ALL
+
+
+
+# More info in book LinuxDominarAdminSistema page 457
